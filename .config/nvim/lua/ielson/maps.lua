2bcd9f26cf24b177c9ab36510cef81f7de2ad2c1
Daniel Mascarenhas <daniel.mascarenhas@saipem.com>
2025-08-09 15:37:24 -0300

    added fzf to vim

--- .config/nvim/lua/ielson/maps.lua
+++ .config/nvim/lua/ielson/maps.lua
@@ -26,9 +26,26 @@ map("n", "<leader>q", "<CMD>q<CR>")
 
 -- clangd
 -- Atalho manual para formatar com o clang-format
-map("n", "<leader>f", function()
+map("n", "<leader>F", function()
   vim.lsp.buf.format({ async = true, buffer = true})
 end, { desc = "LSP: Format buffer" })
+
+
+-- nao funcionando, preciso arrumar
+vim.keymap.set("v", "<leader>f", function()
+  -- pega linha e coluna de início e fim da seleção visual
+  local start_line = vim.fn.line("'<") + 1
+  local start_col  = vim.fn.col("'<")  + 1
+  local end_line   = vim.fn.line("'>") + 1
+  local end_col    = vim.fn.col("'>")  + 1
+
+  vim.lsp.buf.format({
+    range = {
+      start = { start_line, start_col },
+      ["end"] = { end_line,   end_col   },
+    },
+  })
+end, { desc = "LSP: format selection" })
 
 map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to definition" })
 map("n", "K",  vim.lsp.buf.hover,      { desc = "LSP: Hover" })
@@ -36,4 +53,14 @@ map("n", "gr", vim.lsp.buf.references, { desc = "LSP: References" })
 map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename" })
 map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
 
+
+-- mapeamentos para comandos FZF
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap('n', '<leader>f', ':Files<CR>', opts)
+--vim.api.nvim_set_keymap('n', '<leader>b', ':Buffers<CR>', opts)
+--vim.api.nvim_set_keymap('n', '<leader>l', ':Lines<CR>', opts)
+--vim.api.nvim_set_keymap('n', '<leader>L', ':BLines<CR>', opts)
+--vim.api.nvim_set_keymap('n', '<leader>h', ':History<CR>', opts)
+--vim.api.nvim_set_keymap('n', '<leader>c', ':Commits<CR>', opts)
+--vim.api.nvim_set_keymap('n', '<leader>C', ':BCommits<CR>', opts)
 
