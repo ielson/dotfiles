0e68524fb4f1edd3500313583f539f5f17597991
Dan Mascarenhas <danmascandrade@gmail.com>
2026-05-28 09:54:05 -0300

    added ripgrep to fzf on nvim

--- .config/nvim/lua/ielson/maps.lua
+++ .config/nvim/lua/ielson/maps.lua
@@ -12,10 +12,10 @@ map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Mostrar erro/warning"
 -- map("n", "[d", vim.diagnostic.goto_prev,   { desc = "Diagnóstico anterior" })
 -- enviar para loclist e abrir
 
-map("n", "<leader>E", function()
-  vim.diagnostic.setloclist()
-  vim.cmd("lopen")
-end, { desc = "Lista de diagnósticos" })
+-- map("n", "<leader>E", function()
+--   vim.diagnostic.setloclist()
+--   vim.cmd("lopen")
+-- end, { desc = "Lista de diagnósticos" })
 
 -- os <CMD> representam a gente ter apertado : para entrar na linha de comando
 -- só salva se o arquivo tiver alteração, ao contrário do :w
@@ -25,38 +25,39 @@ map("n", "<leader>w", "<CMD>update<CR>")
 map("n", "<leader>q", "<CMD>q<CR>")
 
 -- clangd
--- Atalho manual para formatar com o clang-format
-map("n", "<leader>F", function()
-  vim.lsp.buf.format({ async = true, buffer = true})
-end, { desc = "LSP: Format buffer" })
+-- atalho manual para formatar com o clang-format
+-- map("n", "<leader>f", function()
+--   vim.lsp.buf.format({ async = true, buffer = true})
+-- end, { desc = "lsp: format buffer" })
 
 
 -- nao funcionando, preciso arrumar
-vim.keymap.set("v", "<leader>f", function()
-  -- pega linha e coluna de início e fim da seleção visual
-  local start_line = vim.fn.line("'<") + 1
-  local start_col  = vim.fn.col("'<")  + 1
-  local end_line   = vim.fn.line("'>") + 1
-  local end_col    = vim.fn.col("'>")  + 1
+-- vim.keymap.set("v", "<leader>f", function()
+--   -- pega linha e coluna de início e fim da seleção visual
+--   local start_line = vim.fn.line("'<") + 1
+--   local start_col  = vim.fn.col("'<")  + 1
+--   local end_line   = vim.fn.line("'>") + 1
+--   local end_col    = vim.fn.col("'>")  + 1
+--
+--   vim.lsp.buf.format({
+--     range = {
+--       start = { start_line, start_col },
+--       ["end"] = { end_line,   end_col   },
+--     },
+--   })
+-- end, { desc = "lsp: format selection" })
 
-  vim.lsp.buf.format({
-    range = {
-      start = { start_line, start_col },
-      ["end"] = { end_line,   end_col   },
-    },
-  })
-end, { desc = "LSP: format selection" })
-
-map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to definition" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to Definition" })
 map("n", "K",  vim.lsp.buf.hover,      { desc = "LSP: Hover" })
 map("n", "gr", vim.lsp.buf.references, { desc = "LSP: References" })
 map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename" })
-map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: code Action" })
 
 
--- mapeamentos para comandos FZF
+-- mapeamentos para comandos fzf
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<leader>f', ':Files<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>rg', ':Rg<CR>', opts)
 --vim.api.nvim_set_keymap('n', '<leader>b', ':Buffers<CR>', opts)
 --vim.api.nvim_set_keymap('n', '<leader>l', ':Lines<CR>', opts)
 --vim.api.nvim_set_keymap('n', '<leader>L', ':BLines<CR>', opts)
@@ -69,27 +70,27 @@ vim.api.nvim_set_keymap('n', '<leader>f', ':Files<CR>', opts)
 -- se estiver no modo de diff, usa o padrao do vim, se nao, chama git singgs
 -- c vem de change
 map('n', ']c', function()
-      if vim.wo.diff then
-        vim.cmd.normal({']c', bang = true})
-      else
-        require('gitsigns').nav_hunk('next')
-      end
-    end)
+  if vim.wo.diff then
+    vim.cmd.normal({']c', bang = true})
+  else
+    require('gitsigns').nav_hunk('next')
+  end
+end)
 
-    map('n', '[c', function()
-      if vim.wo.diff then
-        vim.cmd.normal({'[c', bang = true})
-      else
-        require('gitsigns').nav_hunk('prev')
-      end
-    end)
+map('n', '[c', function()
+  if vim.wo.diff then
+    vim.cmd.normal({'[c', bang = true})
+  else
+    require('gitsigns').nav_hunk('prev')
+  end
+end)
 
-    -- Actions
-    map('n', '<leader>hs', function() require('gitsigns').stage_hunk() end)
-    map('n', '<leader>hr', function() require('gitsigns').reset_hunk() end)
-    map('n', '<leader>hp', function() require('gitsigns').preview_hunk_inline() end)
+-- actions
+map('n', '<leader>hs', function() require('gitsigns').stage_hunk() end)
+map('n', '<leader>hr', function() require('gitsigns').reset_hunk() end)
+map('n', '<leader>hp', function() require('gitsigns').preview_hunk_inline() end)
 
--- Diffview toggle
+-- diffview toggle
 local function toggle_diffview(cmd)
   local ok, lib = pcall(require, "diffview.lib")
 
